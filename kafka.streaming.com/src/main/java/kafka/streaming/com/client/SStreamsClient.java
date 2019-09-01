a07f3ea62a99d01e8ad430be1f8e60fea15f9d29
Aqib_Javed <aqibbutt3078@gmail.com>
2019-09-02 00:34:23 +0200

    no message

--- kafka.streaming.com/src/main/java/kafka/streaming/com/client/SStreamsClient.java
+++ kafka.streaming.com/src/main/java/kafka/streaming/com/client/SStreamsClient.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 
 public class SStreamsClient {
@@ -19,7 +20,7 @@ public class SStreamsClient {
 	private static final String BROKER_DOMAIN_NAME = "sandbox-hdp.hortonworks.com";
 	private static final String SPARK_SQL_STREAMING_CHECKPOINT_LOCATION_CONFIG = "spark.sql.streaming.checkpointLocation";
 	private static final String SPARK_SQL_STREAMING_CHECKPOINT_LOCATION = "/user/sparktest/checkpoints";
-	private static String TOPIC_NAME = "BKT_TOPIC";
+	private static String TOPIC_NAME = "MTKAF";
 	private Dataset<Row> datasets;
 	private long start;
 
@@ -55,7 +56,8 @@ public class SStreamsClient {
 	public SStreamsClient writeDataSetsToHDFS() throws StreamingQueryException {
 //		.trigger(Trigger.ProcessingTime(0))
 		Dataset<Row> s = datasets.selectExpr("CAST(value AS STRING)");
-		s.writeStream().format("csv").option("path", URL_HDFS_FILE).start().awaitTermination();
+		s.writeStream().format("csv").option("path", URL_HDFS_FILE).outputMode(OutputMode.Append()).start()
+				.awaitTermination();
 		long end = System.currentTimeMillis();
 		System.out.println("Time after completing streaming [" + ((end - start) / 1000) + "] seconds");
 		return this;
